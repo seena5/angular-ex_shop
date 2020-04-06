d5937ac24b365d15b3567af04020a4e0ed7425a1
seena5 <seenabr5@gmail.com>
2020-04-06 16:54:59 +0900

    데이터 다루기

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,26 +11,32 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
+
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductDetailsComponent,
     ProductAlertsComponent,
-    CartComponent
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
   ],
   bootstrap: [ AppComponent ],
-  providers: [CartService]
+  // providers: [CartService]
 })
 export class AppModule { }
 
